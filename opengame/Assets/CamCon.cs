68b7444357218b76ded305abe2fa132f43c819ad
rikutkb <36298176+rikutkb@users.noreply.github.com>
2018-10-02 23:14:53 +0900

    カメラワークがおかしい
    カメラワークがおかしいので修正をする、あと今後キー入力によって向きを変えるので、updateの部分に入力方向によってplayerの向いている方向の図
    を変更するようにする。

---
+++ opengame/Assets/CamCon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCon : MonoBehaviour {
+    [SerializeField]
+    GameObject player;
+    private Vector3 offset;
+    Vector3 targetcamPos;
+	// Use this for initialization
+	void Start () {
+        offset = player.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+
+
+    private void FixedUpdate()
+    {
+        targetcamPos = player.transform.position + offset;
+
+        transform.position = targetcamPos;
+        transform.LookAt(player.transform);
+    }
+}
---
+++ opengame/Assets/PlayerCon.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCon : MonoBehaviour
+{
+    private Rigidbody rigid;
+    Vector3 Normal_g;
+    [SerializeField]
+    private float speed;
+    RaycastHit hit;
+    // Use this for initialization
+    void Start()
+    {
+        rigid = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frameds
+
+    private void FixedUpdate()
+    {
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        rigid.velocity=(-transform.forward*z+transform.right*x);
+        if (Physics.Raycast(transform.position, -transform.up, out hit, float.PositiveInfinity))//球体に添わせるために開店をする
+        {
+            Quaternion q = Quaternion.FromToRotation(transform.up, hit.normal);
+            transform.rotation *= q;
+        }
+    }
+  
+}
