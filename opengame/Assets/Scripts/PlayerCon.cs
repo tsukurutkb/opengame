2efbf2e4ec90f20a03e66abe123f85f24b66d636
riku iwasaki <s1611426@u.tsukuba.ac.jp>
2018-10-08 19:35:09 +0900

    移動方法の変更

--- opengame/Assets/Scripts/PlayerCon.cs
+++ opengame/Assets/Scripts/PlayerCon.cs
@@ -8,14 +8,7 @@ public class PlayerCon : MonoBehaviour
     Vector3 Normal_g;
     [SerializeField]
     private float speed;
-    [SerializeField]
-    private Sprite front;
-    [SerializeField]
-    private Sprite back;
-    [SerializeField]
-    private Sprite right;
-    [SerializeField]
-    private Sprite left;
+ 
     RaycastHit hit;
     // Use this for initialization
     void Start()
@@ -27,16 +20,24 @@ public class PlayerCon : MonoBehaviour
 
     private void FixedUpdate()
     {
-
+        Vector3 forward_vec=transform.forward;
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-        rigid.velocity=(transform.forward*z+transform.right*x);
+        rigid.velocity=(transform.forward*z);
+        Quaternion rot;
+        rot=Quaternion.AngleAxis(x,transform.up);
+        transform.rotation*=rot;
         if (Physics.Raycast(transform.position, -transform.up, out hit, float.PositiveInfinity))//球体に添わせるために開店をする
         {
-            Quaternion q = Quaternion.FromToRotation(transform.up, hit.normal);
-            transform.rotation *= q;
+            Vector3 projected_vec=Vector3.ProjectOnPlane(transform.forward,hit.normal);
+            projected_vec.Normalize();
+            Debug.Log(projected_vec);
+            transform.rotation=Quaternion.LookRotation(projected_vec,hit.normal);
+           // Quaternion q = Quaternion.FromToRotation(transform.up, hit.normal);
+           // transform.rotation *= q;
         }
+
     }
   
 }
