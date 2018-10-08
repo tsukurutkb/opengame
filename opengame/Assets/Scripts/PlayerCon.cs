c867b90ed6d50d60f1796a2ae0190e51ba114682
riku iwasaki <s1611426@u.tsukuba.ac.jp>
2018-10-08 22:09:29 +0900

    アニメーションの修正

--- opengame/Assets/Scripts/PlayerCon.cs
+++ opengame/Assets/Scripts/PlayerCon.cs
@@ -20,29 +20,20 @@ public class PlayerCon : MonoBehaviour
 
     private void FixedUpdate()
     {
+
+        WalkonSphere();
+    }
+
+    void WalkonSphere(){
         Vector3 vec_normal=transform.position;
         vec_normal.Normalize();
         Vector3 forward_vec=transform.forward;
-
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         rigid.velocity=(transform.forward*z);
-        //Quaternion rot;
-        //rot=Quaternion.AngleAxis(x,transform.up);
         transform.Rotate(transform.up,x,Space.World);
         Vector3 projected_vec=Vector3.ProjectOnPlane(transform.forward,vec_normal);
         transform.rotation=Quaternion.LookRotation(projected_vec,vec_normal);
-        /* 
-        if (Physics.Raycast(transform.position, -transform.up, out hit, float.PositiveInfinity))//球体に添わせるために開店をする
-        {
-            Vector3 projected_vec=Vector3.ProjectOnPlane(transform.forward,hit.normal);
-            Debug.DrawRay(transform.position,projected_vec,Color.green);
-
-            Debug.Log(projected_vec);
-            transform.rotation=Quaternion.LookRotation(projected_vec,hit.normal);
-           // Quaternion q = uaternion.FromToRotation(transform.up, hit.normal);
-           // transform.rotation *= q;
-        }*/
 
     }
   
