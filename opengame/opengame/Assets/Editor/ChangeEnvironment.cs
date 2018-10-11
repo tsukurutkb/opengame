5ca9bda93fc8335fb3373d620141e133fd4c1ab1
riku iwasaki <s1611426@u.tsukuba.ac.jp>
2018-10-11 14:16:25 +0900

    ネットワークの処理をどうするか

---
+++ opengame/opengame/Assets/Editor/ChangeEnvironment.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class Constants {
+#if RELEASE
+    public const string Environment = "本番環境";
+    public const string Host = "honban.domain.come";
+    public const bool IsDevelop = false;
+#elif DEVELOP
+    public const string Environment = "開発環境";
+    public const string Host = "kaihatsu.domain.come";
+    public const bool IsDevelop = true;
+#endif
+}
+public class ChangeEnvironment {
+
+    // 開発環境に切り替え
+    [MenuItem("Environment/Develop")]
+    static void Develop(){
+        var symbols = GetSymbols();
+        symbols.Add("DEVELOP");
+        symbols.Remove("RELEASE");
+        SetSymbols(symbols);
+    }
+
+    // 本番環境に切り替え
+    [MenuItem("Environment/Release")]
+    static void Release(){
+        var symbols = GetSymbols();
+        symbols.Add("RELEASE");
+        symbols.Remove("DEVELOP");
+        SetSymbols(symbols);
+    }
+
+    // 設定されているシンボル定義を取得する
+    static List<string> GetSymbols(){
+        return PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup).Split(';').ToList();
+    }
+
+    // シンボル定義をセットする
+    static void SetSymbols(List<string> symbols){
+        var symbolStr = string.Empty;
+        symbols.ForEach(s => symbolStr += s + ";");
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, symbolStr);
+    }
+}
--- opengame/opengame/Assets/Scripts/PlayerCon.cs
+++ opengame/opengame/Assets/Scripts/PlayerCon.cs
@@ -1,7 +1,54 @@
-﻿using System.Collections;
+﻿
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if DEVELOP
+public class PlayerCon : MonoBehaviour
+{
+    private Rigidbody rigid;
+    Vector3 Normal_g;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Vector3 pos;
+    
+    RaycastHit hit;
+    // Use this for initialization
+    void Start()
+    {
+        transform.position=pos;
+        rigid = this.GetComponent<Rigidbody>();
+        Camera.main.GetComponent <CamCon> ().player = transform.Find("cam").gameObject;
+
+    }
+
+ 
+
+    // Update is called once per frameds
+
+    private void FixedUpdate()
+    {
+        WalkonSphere();
+    }
+
+    void WalkonSphere(){
+        Vector3 vec_normal=transform.position;
+        vec_normal.Normalize();
+        Vector3 forward_vec=transform.forward;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        rigid.velocity=(transform.forward*z);
+        transform.Rotate(transform.up,x,Space.World);
+        Vector3 projected_vec=Vector3.ProjectOnPlane(transform.forward,vec_normal);
+        transform.rotation=Quaternion.LookRotation(projected_vec,vec_normal);
+
+    }
+  
+}
+#elif DEVELOP
 using UnityEngine.Networking;
+
 public class PlayerCon : NetworkBehaviour
 {
     private Rigidbody rigid;
@@ -48,3 +95,4 @@ public override void OnStartLocalPlayer() {
     }
   
 }
+#endif
